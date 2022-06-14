6a9afbd1aac9b5b9f86ce55a331a54f09003bf1a
GeorgiArn <georgearnaudov1@gmail.com>
2022-06-15 02:25:24 +0300

    Change project structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '../.env' });
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-	res.status(200).send({ message: 'stonks go brrrr' });
+	res.status(200).send({ message: `stonks go brrrr` });
 });
 
 const PORT = process.env.PORT;
