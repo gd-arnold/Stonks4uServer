110c80d8bb09f4a6bf95648883ffecca1071e082
GeorgiArn <georgearnaudov1@gmail.com>
2022-06-15 01:13:32 +0300

    Set up basic express

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,2 +1,13 @@
-console.log('hello ladies');
-console.log(process.env.NODE_ENV);
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).send({ message: 'stonks go brrrr' });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`UP & RUNNING ON PORT ${PORT}`));
