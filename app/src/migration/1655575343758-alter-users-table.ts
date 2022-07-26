60a9417b3474a2415d4081aea10f9cf927901a0b
GeorgiArn <georgearnaudov1@gmail.com>
2022-07-26 20:22:39 +0300

    Save createdAt and updatedAt timestamps in UTC

--- app/src/migration/1655575343758-alter-users-table.ts
+++ app/src/migration/1655575343758-alter-users-table.ts
@@ -4,8 +4,12 @@ export class alterUsersTable1655575343758 implements MigrationInterface {
 	name = 'alterUsersTable1655575343758';
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`ALTER TABLE "users" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
-		await queryRunner.query(`ALTER TABLE "users" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
+		await queryRunner.query(
+			`ALTER TABLE "users" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc')`
+		);
+		await queryRunner.query(
+			`ALTER TABLE "users" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc')`
+		);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
--- app/src/migration/1658846659098-create-statements-table.ts
+++ app/src/migration/1658846659098-create-statements-table.ts
@@ -8,7 +8,7 @@ export class createStatementsTable1658846659098 implements MigrationInterface {
 			`CREATE TYPE "public"."statements_type_enum" AS ENUM('income', 'expense')`
 		);
 		await queryRunner.query(
-			`CREATE TABLE "statements" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "type" "public"."statements_type_enum" NOT NULL, "amount" numeric(11,2) NOT NULL, "required_process" integer NOT NULL, "date" TIMESTAMP NOT NULL, "automaticPayment" boolean NOT NULL, "recurrenceRule" text NOT NULL, "categoryId" uuid, "recurringTypeId" uuid, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_7f53bcddeb706df7ea7eec10b8d" PRIMARY KEY ("id"))`
+			`CREATE TABLE "statements" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "type" "public"."statements_type_enum" NOT NULL, "amount" numeric(11,2) NOT NULL, "required_process" integer NOT NULL, "date" TIMESTAMP NOT NULL, "automaticPayment" boolean NOT NULL, "recurrenceRule" text NOT NULL, "categoryId" uuid, "recurringTypeId" uuid, "createdAt" TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'), "updatedAt" TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'), CONSTRAINT "PK_7f53bcddeb706df7ea7eec10b8d" PRIMARY KEY ("id"))`
 		);
 		await queryRunner.query(
 			`ALTER TABLE "statements" ADD CONSTRAINT "FK_a06f03de01a64899de3616cf1d3" FOREIGN KEY ("categoryId") REFERENCES "statement_categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
